124b70b804ce12cbad52870bfeb9003e8048518a
Marko Jokinen <phat.le@thephatle.dev>
2026-04-11 05:27:11 +0300

    updated packages to be isntalled

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,65 @@ dnf5 -y copr enable lionheartp/Hyprland
 # sddm: So you can actually log in
 # noctalia-hyprland-meta: The entire Wayland/Hyprland core ecosystem
 # kitty: Your lifeline to install Nix tomorrow
-dnf5 install -y \
+RUN dnf5 install -y \
+    # Session & Portals (Missing in screenshot)
     sddm \
+    uwsm \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal-gtk \
+    \
+    # The Core + Explicitly forcing the missing daemons
     noctalia-hyprland-meta \
-
-### 3. Disable the COPR
+    hyprpolkitagent \
+    hyprpaper \
+    hyprland-plugins \
+    hyprland-contrib \
+    hyprsysteminfo \
+    hyprsunset \
+    hyprshot \
+    hyprwire \
+    hyprlauncher \
+    \
+    # Clipboard & Screenshots
+    cliphist \
+    wl-clipboard \
+    grim \
+    slurp \
+    satty \
+    \
+    # Theming & GUI Fixes
+    nwg-look \
+    qt6ct \
+    waypaper \
+    \
+    # File Management & System UI
+    nautilus \
+    gvfs-mtp \
+    udiskie \
+    network-manager-applet \
+    blueman \
+    pavucontrol \
+    brightnessctl \
+    playerctl \
+    \
+    # Auth & Secrets 
+    gnome-keyring \
+    libsecret \
+    \
+    # Display Utilities
+    wlr-randr \
+    kanshi \
+    \
+    # Fonts
+    jetbrains-mono-fonts \
+    fontawesome5-fonts \
+    google-noto-fonts-all \
+    \
+    # CLI Power Tools
+    yazi \
+    jq \
+    socat \
+    unzip### 3. Disable the COPR
 dnf5 -y copr disable lionheartp/Hyprland
 
 ### 4. Enable SDDM
