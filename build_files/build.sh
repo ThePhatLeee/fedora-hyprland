0dabca2c4b258ea59400c9bd3b1fbcc78502aed1
Phát Lê <phat.le@thephatle.dev>
2026-04-11 06:46:17 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,18 +1,41 @@
-#!/bin/bash
-
 set -ouex pipefail
 
 ### 1. Enable the active lionheartp Hyprland COPR
 dnf5 -y copr enable lionheartp/Hyprland
 
-### 2. Install the absolute bare minimum
-# sddm: So you can actually log in
-# noctalia-hyprland-meta: The entire Wayland/Hyprland core ecosystem
-# kitty: Your lifeline to install Nix tomorrow
-RUN dnf5 install -y \
-     noctalia-hyprland-meta \
+### 2. Install the system base + Noctalia Stack
+dnf5 install -y \
+    sddm \
+    uwsm \
+    xdg-desktop-portal-hyprland \
+    polkit-gnome \
+    swaync \
+    wl-clipboard \
+    network-manager-applet \
+    noctalia-hyprland-meta \
+    hyprpolkitagent \
+    hyprpaper \
+    hypridle \
+    hyprlock \
+    hyprcursor \
+    cliphist \
+    hyprqt6engine \
+    hyprland-qt-support \
+    qt6ct \
+    nwg-look \
+    waypaper \
+    hyprlauncher \
+    hyprshot \
+    hyprpicker \
+    hyprsunset \
+    hyprsysteminfo \
+    hyprpwcenter \
+    hyprshutdown \
+    gpu-screen-recorder
+
 ### 3. Disable the COPR
 dnf5 -y copr disable lionheartp/Hyprland
 
-### 4. Enable SDDM
+### 4. Enable System Units
+systemctl enable podman.socket
 systemctl enable sddm.service
